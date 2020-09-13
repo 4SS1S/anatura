cee97551c6b332ad0f722459db0ac16f393fb1d8
4ss1s <assisguitar23@gmail.com>
2020-09-13 16:10:47 -0300

    Updates

---
+++ precache-manifest.c03f88bd027af3e2c023a54bd4f2e5bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cdd5f93cc6f52904143680487ea2cf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d9009c28cd932f9ec9",
+    "url": "/static/js/2.d743fb24.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d743fb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97377672bfcef5d18e81",
+    "url": "/static/js/main.67f9909a.chunk.js"
+  },
+  {
+    "revision": "cd712fd2863522d3cbc7",
+    "url": "/static/js/runtime-main.1ce086bc.js"
+  }
+]);
